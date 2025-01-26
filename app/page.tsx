c97dc3304d5280d13a32b3c29beead055a6b089a
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2025-01-26 04:46:50 +0100

    added patient form to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import PatientForm from "@/components/form/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="w-full flex h-screen max-h-screen ">
+      {/* TODO: OTP verification / PasskeyModals */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -13,8 +16,24 @@ export default function Home() {
             width={1000}
             className="mb-12 h-10 w-fit"
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between font-jakarta">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2024 Remedic
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
     </main>
   );
 }
